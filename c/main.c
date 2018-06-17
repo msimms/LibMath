ebeb4522d0a65964cc23a29c41569c3e626baa9f
Mike Simms <msimms01@gmail.com>
2018-06-17 15:09:58 -0400

    Added an XCode project file.

---
+++ c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  libmath
+//
+//  Created by Michael Simms on 6/17/18.
+//  Copyright © 2018 Michael Simms. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
