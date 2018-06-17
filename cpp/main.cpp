35a8038b20993c411417ceee759b206742d0b63b
Mike Simms <msimms01@gmail.com>
2018-06-17 13:47:26 -0400

    Added an XCode project file.

---
+++ cpp/main.cpp
@@ -0,0 +1,9 @@
+// Created by Michael Simms on 6/17/18.
+// Copyright (c) 2018 Michael Simms. All rights reserved.
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+	return 0;
+}
