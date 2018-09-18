98145481a4d9a7e2d6b9e5088e19ad05930c74a2
Mike Simms <msimms01@gmail.com>
2018-09-17 20:04:42 -0400

    Fixed compile warning, re: printf.

--- c/main.c
+++ c/main.c
@@ -74,7 +74,7 @@ int main(int argc, const char * argv[])
 	printf("---------------\n");
 
 	size_t distance = hammingDistance("1011101", "1001001");
-	printf("Hamming Distance: %u\n", distance);
+	printf("Hamming Distance: %zu\n", distance);
 	
 	return 0;
 }
