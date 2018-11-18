1ee2961f1785d42e9f672c96b475e82a9ccd3aeb
Mike Simms <msimms01@gmail.com>
2018-11-17 19:10:44 -0500

    Added basic peak finding algorithm for python.

--- python/main.py
+++ python/main.py
@@ -20,13 +20,48 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 
+import argparse
+import csv
 import random
 import distance
 import kmeans
 import math
+import peaks
 import statistics
+import sys
+
+def read_csv(csv_file_name):
+    columns = []
+    ts_list = []
+    x_list = []
+    y_list = []
+    z_list = []
+
+    with open(csv_file_name) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            ts_list.append(float(row[0]))
+            x_list.append(float(row[1]))
+            y_list.append(float(row[2]))
+            z_list.append(float(row[3]))
+
+	columns.append(ts_list)
+	columns.append(x_list)
+	columns.append(y_list)
+	columns.append(z_list)
+	return columns
 
 def main():
+    # Parse command line options.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--csv_file_name", default="", help="CSV file containing test peak data", required=False)
+
+    try:
+        args = parser.parse_args()
+    except IOError as e:
+        parser.error(e)
+        sys.exit(1)
+
     #print("Distance Tests:")
     #print("---------------")
 
@@ -63,6 +98,23 @@ def main():
     kMeansIn[9] = 0.001
     tags = kmeans.kmeans_equally_space_centroids_1_d(kMeansIn, 3, 0.001, 3)
     print(tags)
+
+    if len(args.csv_file_name) > 0:
+        print("\nPeak Finding Tests:")
+        print("-------------------")
+
+        csv_data = read_csv(args.csv_file_name)
+        csv_data = csv_data[1:]
+        axis_count = 0
+        for csv_column in csv_data:
+            print("Axis " + str(axis_count) + ":")
+            axis_count = axis_count + 1
+
+            peak_count = 0
+            peak_list = peaks.find_peaks_in_numeric_array(csv_column, 2.0)
+            for peak in peak_list:
+                print("Peak " + str(peak_count) + ": {" + str(peak.left_trough.x) + ", " + str(peak.peak.x) + ", " + str(peak.right_trough.x) + ", " + str(peak.area) + "}")
+                peak_count = peak_count + 1
 
 if __name__ == "__main__":
     main()
---
+++ python/peaks.py
@@ -0,0 +1,120 @@
+#  MIT License
+#
+#  Copyright (c) 2018 Michael J Simms. All rights reserved.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
+import statistics
+
+class GraphPoint(object):
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def clear(self):
+        self.x = 0
+        self.y = 0.0
+
+class GraphPeak(object):
+
+    def __init__(self):
+        self.left_trough = GraphPoint(0, 0.0)
+        self.peak = GraphPoint(0, 0.0)
+        self.right_trough = GraphPoint(0, 0.0)
+        self.area = 0.0
+
+    def clear(self):
+        self.left_trough.clear()
+        self.peak.clear()
+        self.right_trough.clear()
+        self.area = 0.0
+
+def compute_area_from_graph_point_array(data, current_peak):
+    current_peak.area = 0.0
+
+    if current_peak.left_trough.x < current_peak.right_trough.x:
+        prev_point = data[current_peak.left_trough.x]
+        for current_point in data[current_peak.left_trough.x + 1:current_peak.right_trough.x + 1]:
+            b = current_point.y + prev_point.y
+            current_peak.area = current_peak.area + (0.5 * b)
+            prev_point = current_point
+
+def compute_area_from_numeric_array(data, current_peak):
+    current_peak.area = 0.0
+
+    if current_peak.left_trough.x < current_peak.right_trough.x:
+        prev_point = data[current_peak.left_trough.x]
+        for current_point in data[current_peak.left_trough.x + 1:current_peak.right_trough.x + 1]:
+            b = current_point + prev_point
+            current_peak.area = current_peak.area + (0.5 * b)
+            prev_point = current_point
+
+def find_peaks_in_numeric_array(data, sigmas):
+    peaks = []
+    current_peak = GraphPeak()
+
+    mean = statistics.mean(data)
+    stddev = sigmas * statistics.stddev(data, mean)
+    threshold = mean + stddev
+
+    x = 0
+
+    for y in data:
+        if y < threshold:
+
+            # Have we found a peak? If so, add it and start looking for the next one.
+            if current_peak.right_trough.x > 0:
+                compute_area_from_numeric_array(data, current_peak)
+                peaks.append(current_peak)
+                current_peak = GraphPeak()
+
+            # Are we looking for a left trough?
+            elif current_peak.left_trough.x == 0:
+                current_peak.left_trough.x = x
+                current_peak.left_trough.y = y
+            
+            # If we have a left trough and an existing peak, assume this is the right trough - for now.
+            elif (current_peak.peak.x > current_peak.left_trough.x) and (current_peak.left_trough.x > 0):
+                current_peak.right_trough.x = x
+                current_peak.right_trough.y = y
+
+            else:
+                current_peak.left_trough.x = x
+                current_peak.left_trough.y = y
+
+        elif current_peak.left_trough.x > 0: # Left trough is set.
+
+            # Are we looking for a peak or is this bigger than the current peak?
+            if current_peak.peak.x == 0 or y >= current_peak.peak.y:
+                current_peak.peak.x = x
+                current_peak.peak.y = y
+
+        elif current_peak.right_trough.x > 0: # Right trough is set.
+            compute_area_from_numeric_array(data, current_peak)
+            peaks.append(current_peak)
+            current_peak = GraphPeak()
+
+        else: # Nothing is set, but the value is above the threshold.
+            current_peak.left_trough.x = x
+            current_peak.left_trough.y = y
+
+        x = x + 1
+
+    return peaks
