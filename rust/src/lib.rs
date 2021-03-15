9312a17362bd3c3355a0f7bd83e9241cdd9a11b8
Mike Simms <msimms01@gmail.com>
2021-03-14 20:50:24 -0400

    Added lib.rs.

---
+++ rust/src/lib.rs
@@ -0,0 +1,11 @@
+#[crate_id = "lib_math"];
+#[crate_type = "lib"];
+
+pub mod distance;
+pub mod statistics;
+pub mod kmeans;
+pub mod peaks;
+pub mod powers;
+pub mod square_matrix;
+pub mod statistics;
+pub mod vector;
